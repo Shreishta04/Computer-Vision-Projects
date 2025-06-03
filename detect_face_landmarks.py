ebdb45e41d5f0d99fa783ffd41fa704c230ac5a7
Shreishta Manoj <shreishtamanoj04@gmail.com>
2025-06-03 14:45:22 +0530

    initial commit

---
+++ detect_face_landmarks.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
+
+#create object of VideoCapture
+cap = cv2.VideoCapture(0) # 0 is the default cam
+
+while True:
+    #to capture the image we use read() and it returns key and img
+    key,img = cap.read()
+
+    # objects captured using cv is in BGR, we use cvtColor to convert this BGR to RGB
+    rgb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #face landmarks will be stored in results
+    results = face_mesh.process(rgb_frame)
+
+    #multiple landmarks on the face will be shown
+    if results.multi_face_landmarks:
+        for landmarks in results.multi_face_landmarks:
+            for point in landmarks.landmark:
+                x, y = int(point.x * img.shape[1]), int(point.y * img.shape[0])
+                cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
+
+    #display the image in a window
+    cv2.imshow("my_video",img)
+    #it constantly captures image by image so we wait a second to keep it from getting stuck
+    cv2.waitKey(1)
---
+++ emotion_detect.py
@@ -0,0 +1,22 @@
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret,img = cap.read()
+
+    results = DeepFace.analyze(img, actions=["emotion"],enforce_detection = False)
+
+    emotion = results[0]['dominant_emotion']
+
+    #text, org, font,font scale,color,thickness
+    cv2.putText(img, f'Emotion: {emotion}',(50,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+
+    cv2.imshow("Emotion Recognition",img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
